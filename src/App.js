4bbbefa95fe59040266264eec89fb5b2737e3905
devzdevelop <devbusinessinfluencer@gmail.com>
2024-07-04 07:19:45 -0500

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import "./styles.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+// import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="app">
+      <Header />
+      <Main />
     </div>
   );
 }
