553f50d274fa7a436da57ac61423f095acbbf84e
Devin Brown <54863017+devzdevelop@users.noreply.github.com>
2024-07-05 12:20:55 +0000

    setting up some boiler plate

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import {useEffect, useReducer} from 'react';
+
 import Header from "./components/Header";
 import Main from "./components/Main";
 // import "./styles.css";
 
+const initialState = {
+  questions: [],
+  // "loading", "error", "ready", "active", "finished"
+  status: "loading"
+}
+
+const reducer = (state, action) => {
+  
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  
+  useEffect(() => {
+    console.log("Fetching");
+    
+    const fetchQuestions = async () => {
+      try{
+        const res = await fetch('https://82f5jm-8000.csb.app/questions');
+        const data = await res.json();
+        console.log("questions: ", data);
+      } catch(e) {
+        console.log("error: ", e);
+      }
+    }
+    
+    fetchQuestions();
+  },[]);
+  
   return (
     <div className="app">
       <Header />
